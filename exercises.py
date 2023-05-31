4248bbcd7e6e8a6d4c89567ee80a062852c8b5d4
Victor <victor181851@gmail.com>
2023-05-30 21:36:55 -0600

    Feat: A tuple was added
    A tuple was added, with the numbers from 1 to 7

--- exercises.py
+++ exercises.py
@@ -10,3 +10,14 @@ names.append("Jaime")
 #Print values from the name list
 for name in names:
     print(name)
+
+#Initialize a tuple with numbers from 1 to 7
+numbers = (1,2,3,4,5,6,7)
+
+
+i = 0
+
+#Print values from the tuple of numbers
+while i < len(numbers):
+    print(numbers[i])
+    i = i + 1
