4540aeb5d55a7bfd9b8018972f0b487dd6502ff1
Victor <victor181851@gmail.com>
2023-05-30 21:14:24 -0600

    Feat: New changes added
    A list of people's names was added and one more name was added to that list.

---
+++ exercises.py
@@ -0,0 +1,12 @@
+
+#Initialize a name list
+names = ["Victor","Maximo","Ivan","Saul","Daniel"]
+
+print(names)
+
+#Add another name to the name list
+names.append("Jaime")
+
+#Print values from the name list
+for name in names:
+    print(name)
